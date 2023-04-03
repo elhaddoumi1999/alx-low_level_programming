f47b33daac939e9e93f0274d6de29d713d7d16c4
elhaddoumi1999 <elhaddoumi99@gmail.com>
2023-04-03 16:34:10 -0700

    0. memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,9 +7,6 @@
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x07-pointers_arrays_strings directory.
  */
-
-#include <stddef.h>
-
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
 void *_memcpy(void *dest, const void *src, size_t n);
