eb1b306a8bd02118805faed2ce88fbaf7ceaf507
elhaddoumi1999 <elhaddoumi99@gmail.com>
2023-04-09 21:10:48 -0700

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,82 +1,51 @@
 #include <stdio.h>
-                
 #include <stdlib.h>
-                
-#include "main.h"
-                
-                
-/**
-                
- * main - prints the minimum number of coins to
-                
- * make change for an amount of money
-                
- * @argc: number of arguments
-                
- * @argv: array of arguments
-                
- *
-                
- * Return: 0 (Success), 1 (Error)
-                
- */
-                
-int main(int argc, char *argv[])
-                
-{
-                
-        int num, j, result;
-                
-        int coins[] = {25, 10, 5, 2, 1};
-                
-                
-        if (argc != 2)
-                
-        {
-                
-                printf("Error\n");
-                
-                return (1);
-                
-        }
-                
-                
-        num = atoi(argv[1]);
-                
-        result = 0;
-                
-                
-        if (num < 0)
-                
-        {
-                
-                printf("0\n");
-                
-                return (0);
-                
-        }
-                
-                
-        for (j = 0; j < 5 && num >= 0; j++)
-                
-        {
-                
-                while (num >= coins[j])
-                
-                {
-                
-                        result++;
-                
-                        num -= coins[j];
-                
-                }
-                
-        }
-                
-                
-        printf("%d\n", result);
-                
-        return (0);
-                
-}
 
+/**
+ * main - prints the min number of coins to make change
+ * for an amount of money
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int total, count;
+	unsigned int i;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	total = strtol(argv[1], &p, 10);
+	count = 0;
+
+	if (!*p)
+	{
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(cents[i]); i++)
+			{
+				if (total >= cents[i])
+				{
+					count += total / cents[i];
+					total = total % cents[i];
+				}
+			}
+		}
+		if (total == 1)
+			count++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", count);
+	return (0);
+}
