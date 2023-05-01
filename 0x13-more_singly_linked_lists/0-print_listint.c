8061f047fa14e455e7acbd080817d2247b28d769
elhaddoumi1999 <elahddoumi99@gmail.com>
2023-05-01 15:55:04 -0700

    My Tasks 0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
-
-	return (nodes);
+	return (count);
 }
