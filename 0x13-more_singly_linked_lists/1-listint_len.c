80952d9d0327ed00e8a20ed2f7bc4596563005a8
elhaddoumi1999 <elahddoumi99@gmail.com>
2023-05-02 08:17:11 -0700

    0x13. C - More singly linked listsls

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * @copyright EL HADDOUMI
+ * @elhaddoumi
  * listint_len - Returns the number of elements
  *               in a linked listint_t list.
  * @h: A pointer to the head of the listint_t list.
