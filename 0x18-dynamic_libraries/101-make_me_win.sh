b9e409eb90afa9fea06f022a6264dd8883ea87ac
elhaddoumi <elhaddoumi99@gmail.com>
2023-10-17 01:35:41 +0100

    Update  All files ...

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/elhaddoumi1999/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -P /tmp https://github.com/elhaddoumi1999/alx-low_level_programming/raw/master/0x18-dynamic_libraries/task3.so
+export LD_PRELOAD=/tmp/task3.so
